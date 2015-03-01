0b59febbc377949bb0a94d3d588830d5917798b6
rrtigga <rrtigga@ucdavis.edu>
2015-02-28 16:49:48 -0800

    RORO

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/Spicycurryman/Desktop/Hippo/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/Spicycurryman/Desktop/Hippo/platforms/android/CordovaLib/AndroidManifest.xml \
