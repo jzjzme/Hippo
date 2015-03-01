c408672581687b2c8790988b2eda249224a5f5e6
jzjzme <jackiez3895@gmail.com>
2015-02-28 21:04:09 -0800

    facebook

---
+++ www/js/LoginCtrl.js
@@ -0,0 +1,19 @@
+// LoginCtrl.js
+function LoginCtrl(store, $scope, $location, auth) {
+  $scope.login = function() {
+    auth.signin({
+      authParams: {
+        scope: 'openid offline_access',
+        device: 'Mobile device'
+      }
+    }, function(profile, token, accessToken, state, refreshToken) {
+      // Success callback
+      store.set('profile', profile);
+      store.set('token', token);
+      store.set('refreshToken', refreshToken);
+      $location.path('/');
+    }, function() {
+      // Error callback
+    });
+  }
+}
