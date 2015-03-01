84ef026cb5ef277ee92ae2330cb5905644bbfe00
shaibruhis <shaibruhis@gmail.com>
2015-02-28 20:09:42 -0800

    global count of quantity implemented

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,4 +1,35 @@
 angular.module('starter.controllers', [])
+.run(function($rootScope) {
+  $rootScope.totalQuantity = 0;
+})
+
+.controller('quantityCtrl', function($scope, $rootScope) {
+  $scope.quantity = 0;
+  $scope.increment = function(num) {
+    // deal with quantity of individual card 
+    if ($scope.quantity == 0 && num == -1) {
+      return;
+    }
+    else {
+      $scope.quantity += num;
+      $rootScope.totalQuantity += num;
+    }
+
+    // deal with displaying checkout if any card has quantity >= 1
+    if ($rootScope.totalQuantity == 0) {
+      
+    }
+    else if ($rootScope.totalQuantity == 1) {
+
+    }
+    else if ($rootScope.totalQuantity == 2) {
+
+    }
+    else if ($rootScope.totalQuantity == 3) {
+
+    }
+  }
+})
 
 .controller('DashCtrl', function($scope) {})
 
